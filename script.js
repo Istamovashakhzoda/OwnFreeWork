18409cb75d4646b4fb4463319ab2f21b0fe70a03
Istamovashakhzoda <istamovashahzoda@gmail.com>
2023-08-07 17:23:05 +0500

    i take the new time interval

--- script.js
+++ script.js
@@ -18,3 +18,57 @@ const appendZero = (value) => {
     value=value <10 ? `0${value}` :value;
     return value
 }
+
+reset.addEventListener(
+    "click",
+    (resetTime = () =>{
+        pauseTime();
+        count = 59;
+        minCount = 24;
+        time.textContent = `${minCount +1}:00`
+    })
+)
+
+const removeFocus = () =>{
+    buttons.forEach((btn) => {
+        btn.classList.remove("btn-focus")
+    })
+}
+
+focusButton.addEventListener("click", () =>{
+    removeFocus();
+    focusButton.classList.add("btn-focus");
+    pauseTime();
+    minCount = 24;
+    count = 59;
+    time.textContent = `${minCount +1}:00`
+})
+
+shortBreakButton.addEventListener("click", () =>{
+    removeFocus()
+    shortBreakButton.classList.add("btn-focus");
+    pauseTime();
+    minCount =4;
+    count = 59;
+    time.textContent = `${appendZero(minCount + 1)}:00`
+})
+
+longBreakButton.addEventListener("click", () => {
+    removeFocus();
+    longBreakButton.classList.add("btn-focus")
+    pauseTime()
+    minCount =14;
+    count=59;
+    time.textContent = `${minCount +1}:00`
+})
+
+pause.addEventListener(
+    "click",
+    (pauseTime = () =>{
+        pause=true;
+        clearInterval(set)
+        startBtn.classList.remove("hide")
+        pause.classList.remove("show")
+        reset.classList.remove("show")
+    })
+)
