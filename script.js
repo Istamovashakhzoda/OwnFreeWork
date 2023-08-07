fe0924d140b1676151953163a00e19929627983f
Istamovashakhzoda <istamovashahzoda@gmail.com>
2023-08-07 17:02:12 +0500

    we will the start javascript for this site

---
+++ script.js
@@ -0,0 +1,6 @@
+let focusButton = document.getElementById("focus")
+let buttons = document.querySelectorAll(".btn")
+let shortBreakButton = document.getElementById("shortbreak")
+let longBreakButton = document.getElementById("longbreal")
+let startBtn = document. getElementById("btn-start")
+let reset = document.getElementById("btn-reset")
