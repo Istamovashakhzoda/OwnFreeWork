13d8d91db98d03fc4e374d00575d7e2c4584ba4b
Istamovashakhzoda <istamovashahzoda@gmail.com>
2023-08-07 17:08:01 +0500

    javascript take the time count

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ let shortBreakButton = document.getElementById("shortbreak")
 let longBreakButton = document.getElementById("longbreal")
 let startBtn = document. getElementById("btn-start")
 let reset = document.getElementById("btn-reset")
+let pause = document.getElementById("btn-pause")
+let time = document.getElementById("time")
+
+let set;
+let count = 59;
+let paused = true;
+let minCount = 24;
+
+time.textContent = `${minCount + 1}:00`;
+
+const appendZero = (value) => {
+    value=value <10 ? `0${value}` :value;
+    return value
+}
